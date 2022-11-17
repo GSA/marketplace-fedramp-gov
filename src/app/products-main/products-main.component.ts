f6d9bf82cd5ab0f49a90b2a190997231cadec6a3
Adam Vanderhyden <56723511+echovanderhyden@users.noreply.github.com>
2022-11-16 21:51:44 -0500

    Get JSON from local storage and map it to be used.

--- src/app/products-main/products-main.component.ts
+++ src/app/products-main/products-main.component.ts
@@ -5,9 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './products-main.component.html',
   styleUrls: ['./products-main.component.scss']
 })
+
 export class ProductsMainComponent implements OnInit {
 
-  constructor() { }
+  data: any;
+  
+  constructor() { 
+
+    var cacheJson = localStorage.getItem('cacheJson');
+    var parseJson = JSON.parse(cacheJson!);
+    
+    // can parse into data.Products if useful later?
+    this.data = parseJson.data;
+  }
 
   ngOnInit(): void {
   }
