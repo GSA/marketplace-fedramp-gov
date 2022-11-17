a5eaa020a16ef5e8809c2bf7ebc71b0b865ac392
Adam Vanderhyden <56723511+echovanderhyden@users.noreply.github.com>
2022-11-16 21:44:14 -0500

    adding constants file for github data url

---
+++ src/app/app.constants.ts
@@ -0,0 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class AppConstants {
+    readonly GIT_URL: string = 'https://raw.githubusercontent.com/echovanderhyden/test-data/main/adam.json';
+}
