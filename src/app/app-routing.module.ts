b91f037fb280ef305ba2ef4334807149b1f11226
Adam Vanderhyden <56723511+echovanderhyden@users.noreply.github.com>
2022-12-07 20:15:45 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,10 @@ import { AssessorsInfoComponent } from './assessors-info/assessors-info.componen
 import { ProductInfoComponent } from './product-info/product-info.component';
 
 const routes: Routes = [
-   {
-    path: 'product-details/:id',
-    component: ProductInfoComponent,
-   },
-   {
-    path: 'assessors-details/:id',
-    component: AssessorsInfoComponent,
-   },
-   {
-    path: 'agencies-details/:id',
-    component: AgencyInfoComponent,
-   }
+   {path: 'products/:id',    component: ProductInfoComponent },
+   {path: 'assessors/:id',  component: AssessorsInfoComponent},
+   {path: 'agencies/:id',   component: AgencyInfoComponent },
+   {path: '', redirectTo: '/products', pathMatch: 'full'}
 ];
 
 @NgModule({
