dc60d352b4fae77b6968be65ebdd6020d180f015
Adam Vanderhyden <56723511+echovanderhyden@users.noreply.github.com>
2022-12-08 11:25:21 -0500

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,4 +3,5 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class AppConstants {
     readonly GIT_URL: string = 'https://raw.githubusercontent.com/echovanderhyden/test-data/main/adam.json';
+    readonly CACHE_DELAY: number = 1000;
 }
