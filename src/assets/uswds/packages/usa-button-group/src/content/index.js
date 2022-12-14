d6163cabe8d5de2159eec18bd09356c979c1fe4d
michaelfrising <michael.frising@gsa.gov>
2022-12-14 08:15:50 -0500

    try to fix mobile menu

--- src/assets/uswds/packages/usa-button-group/src/content/index.js
+++
@@ -1,2 +0,0 @@
-export { default as DefaultContent } from "./usa-button-group.json";
-export { default as SegmentedContent } from "./usa-button-group~segmented.json";
--- src/assets/uswds/packages/usa-memorable-date/src/usa-memorable-date.stories.js
+++
@@ -1,9 +0,0 @@
-import Component from "./usa-memorable-date.twig";
-
-export default {
-  title: "Components/Form Inputs/Memorable Date",
-};
-
-const Template = (args) => Component(args);
-
-export const MemorableDate = Template.bind({});
