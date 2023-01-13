078a59e7e308cde0d91db51a7bb65673a4a49fbe
Adam Vanderhyden <adam.vanderhyden@echoorigin.com>
2023-01-13 17:12:46 -0500

    text search/filter

--- src/assets/js/filter.js
+++ src/assets/js/filter.js
@@ -1,6 +1,55 @@
+function filterSearch(inVal) {
+
+
+    var clear = document.getElementById("clear-search-desktop");
+    var table = document.getElementById("sort-table-desktop");
+
+    const kids = table.children;
+    var rows = [];
+
+    clear.classList.remove("d-none");
+
+    for (let i = 0; i < kids.length; i++) {
+
+        kids[i].classList.remove("d-search-none");
+
+        if(inVal != '') {
+
+            if (kids[i].textContent.toLowerCase().includes(inVal.toLowerCase()) == false) {
+
+                kids[i].classList.add("d-search-none");
+            }
+        }
+        rows[i] = kids[i].outerHTML;
+    }
+
+    if(inVal == '') {
+        clear.classList.add("d-none");
+    }
+}
+
+function clearSearch() {
+
+    var search = document.getElementById("clear-search");
+    var clear = document.getElementById("clear-search-desktop");
+    var table = document.getElementById("sort-table-desktop");
+
+    search.value = "";
+    clear.classList.add("d-none");
+
+    const kids = table.children;
+    var rows = [];
+
+    for (let i = 0; i < kids.length; i++) {
+        rows[i] = kids[i].outerHTML;
+    }
+
+    table.innerHTML = rows.join('').replace(/d-search-none/g,'');
+}
+
 function filterRows(filterClass, tableType = 'desktop') {
 
-    var clear = document.getElementById("clear-all-" + tableType);
+    var clear = document.getElementById("clear-filters-" + tableType);
     var table = document.getElementById("sort-table-" + tableType);
     var filter = document.getElementById(filterClass);
 
@@ -37,7 +86,7 @@ function filterRows(filterClass, tableType = 'desktop') {
 
 function clearFilter(tableType = 'desktop') {
 
-    var clear = document.getElementById("clear-all-" + tableType);
+    var clear = document.getElementById("clear-filters-" + tableType);
     var table = document.getElementById("sort-table-" + tableType);
 
     const kids = table.children;
