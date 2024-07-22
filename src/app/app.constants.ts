972f5225685619e2234e850b7858c64dced470c9
Adam Vanderhyden <adam.vanderhyden@gsa.gov>
2024-07-22 16:54:11 -0400

    testing signed commit

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 
 @Injectable()
-export class AppConstants { 
+export class AppConstants {
     readonly GIT_URL: string = 'https://raw.githubusercontent.com/GSA/marketplace-fedramp-gov-data/main/test.json';
+    //testing signed commit
     //readonly GIT_URL: string = 'https://raw.githubusercontent.com/GSA/marketplace-fedramp-gov-data/main/data.json';
     readonly CACHE_DELAY: number = 1000;
     readonly CACHE_DATE_FORMAT: string = 'yyyy-MM-dd';
