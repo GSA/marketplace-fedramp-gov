d155a1cda07d53eace3a89f95a60f068ccf2e718
Adam Vanderhyden <adam.vanderhyden@gsa.gov>
2024-07-23 16:42:27 -0400

    parse data.json for production

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,8 +2,7 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AppConstants { 
-    readonly GIT_URL: string = 'https://raw.githubusercontent.com/GSA/marketplace-fedramp-gov-data/main/test.json';
-    //readonly GIT_URL: string = 'https://raw.githubusercontent.com/GSA/marketplace-fedramp-gov-data/main/data.json';
+    readonly GIT_URL: string = 'https://raw.githubusercontent.com/GSA/marketplace-fedramp-gov-data/main/data.json';
     readonly CACHE_DELAY: number = 1000;
     readonly CACHE_DATE_FORMAT: string = 'yyyy-MM-dd';
 }
